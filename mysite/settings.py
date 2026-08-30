4810c6443167ee66df1c4664159d2b0eeb1b3d58
Herczeg László <h657886@stud.u-szeged.hu>
2026-08-30 22:18:51 +0200

    Move SECRET_KEY and DEBUG to environment variables; add migrations

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,19 +11,21 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v9#-9g#ui^m6c(s!*v@ekon77#j_hxp6m2^4)(46yt#&qyut1)'
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = []
 
